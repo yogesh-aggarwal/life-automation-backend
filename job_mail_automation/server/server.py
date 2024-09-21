b68e314f08f8b303cdc610d37f8a1506d4d04126
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-09-21 15:18:55 +0530

    Added health check

--- job_mail_automation/server/server.py
+++ job_mail_automation/server/server.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 app.register_blueprint(google_auth_router, url_prefix="/auth/google")
 
 
+@app.get("/health")
+def health():
+    return "OK", 200
+
+
 def start_server():
     app.run(host="0.0.0.0", port=3000, debug=True, use_reloader=False)
