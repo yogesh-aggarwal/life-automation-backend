d0bde3d99973f46cbc5a8932ed9974ad1feac33a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-09-21 15:50:42 +0530

    Configured CORS

--- job_mail_automation/server/server.py
+++ job_mail_automation/server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from .routes.auth.google.router import google_auth_router
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(google_auth_router, url_prefix="/auth/google")
 
