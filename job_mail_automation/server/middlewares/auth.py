14cfda1972c480876b1be14284e9f82268ca43df
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-09-23 14:42:36 +0530

    Added middleware for firebase authentication

---
+++ job_mail_automation/server/middlewares/auth.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify
+
+from job_mail_automation.core.firebase import auth
+from job_mail_automation.types.user import User
+
+
+def firebase_auth_middleware():
+    firebase_auth_token = request.headers.get("Firebase-Authorization-Token")
+    if not firebase_auth_token:
+        return jsonify({"message": "unauthorized"}), 401
+
+    try:
+        user = auth.verify_id_token(firebase_auth_token)
+        user_email = user["email"]
+
+        try:
+            user = User.get_from_email(user_email)
+        except Exception:
+            return jsonify({"message": "user_not_found"}), 404
+
+        setattr(request, "user", user)
+    except Exception:
+        return jsonify({"message": "unauthorized"}), 401
+
+    return None
